19a8bc2704c1f140602e9297e96d09d54de594e1
mads <anderen72@gmail.com>
2025-08-22 15:33:25 +0200

    implemented slayer rewards

--- scripts/query_wiki_api.py
+++ scripts/query_wiki_api.py
@@ -1,8 +1,5 @@
-import re
-
-import pytest
+import pandas as pd
 import requests
-from requests.utils import quote
 
 s = requests.Session()
 s.headers.update({"user-agent": "madlor"})
@@ -31,142 +28,132 @@ SKILLS = [
     "Construction",
     "Hunter",
 ]
+BASE = "https://oldschool.runescape.wiki/w/"
 
 
-def item(item_name: str) -> dict:
-    """ask osrs wiki api for wikiLink and imgSrc for item"""
-    # default version handles wiki pages with
+def bucket_query(bucket_name: str, name: str):
+    name = name.replace("'", "\\'")  # handle cases like tumeken's shadow
+    query_string = {
+        "infobox_item": f"bucket('infobox_item').select('page_name', 'image').where('page_name','{name}').where('default_version',true).run()",
+        "infobox_spell": f"bucket('infobox_spell').select('page_name', 'image').where('page_name','{name}').run()",
+        "infobox_construction": f"bucket('infobox_construction').select('page_name', 'icon').where('page_name','{name}').run()",
+        "quest": f"bucket('quest').select('page_name').where('page_name','{name}').run()",
+    }
     params = {
         "action": "bucket",
         "format": "json",
-        "query": f"bucket('infobox_item').select('page_name', 'image').where('page_name','{item_name}').where('default_version',true).run()",
+        "query": query_string[bucket_name],
     }
     resp = s.get("https://oldschool.runescape.wiki/api.php", params=params)
     if not resp.ok:
         raise ConnectionError(f"request failed: {resp.status_code}")
-    if not resp.json()["bucket"]:
-        return None  # no results
-    page_name: str = resp.json()["bucket"][0]["page_name"].replace(" ", "_")
-    image_file: str = resp.json()["bucket"][0]["image"][0].replace(" ", "_")
+    return resp.json()["bucket"]
 
-    wiki_url = f"https://oldschool.runescape.wiki/w/{page_name}"
-    image_url = f"https://oldschool.runescape.wiki/w/{image_file}"
+
+def item(item_name: str) -> dict | None:
+    """ask osrs wiki api for wikiLink and imgSrc for item"""
+    bucket = bucket_query("infobox_item", item_name)
+    if not bucket:
+        return None
+    page_name: str = bucket[0]["page_name"].replace(" ", "_")
+    image_file: str = bucket[0]["image"][0].replace(" ", "_")
+
+    wiki_url = BASE + page_name
+    image_url = BASE + image_file
     return {"wikiUrl": wiki_url, "imgUrl": image_url}
 
 
-def spell(spell_name: str) -> dict:
+def spell(spell_name: str) -> dict | None:
     """ask osrs wiki api for wikiLink and imgSrc for spell"""
-
-    params = {
-        "action": "bucket",
-        "format": "json",
-        "query": f"bucket('infobox_spell').select('page_name', 'image').where('page_name','{spell_name}').run()",
-    }
-    resp = s.get("https://oldschool.runescape.wiki/api.php", params=params)
-    if not resp.ok:
-        raise ConnectionError(f"request failed: {resp.status_code}")
-    if not resp.json()["bucket"]:
-        return None  # no results
-    page_name: str = resp.json()["bucket"][0]["page_name"].replace(" ", "_")
+    bucket = bucket_query("infobox_spell", spell_name)
+    if not bucket:
+        return None
+    page_name: str = bucket[0]["page_name"].replace(" ", "_")
     image_file: str = (
-        resp.json()["bucket"][0]["image"]
-        .replace(" ", "_")
-        .replace("[[", "")
-        .replace("]]", "")
+        bucket[0]["image"].replace(" ", "_").replace("[[", "").replace("]]", "")
     )
-    wiki_url = f"https://oldschool.runescape.wiki/w/{page_name}"
-    image_url = f"https://oldschool.runescape.wiki/w/{image_file}"
+    wiki_url = BASE + page_name
+    image_url = BASE + image_file
     return {"wikiUrl": wiki_url, "imgUrl": image_url}
 
 
-def construction(input):
-    """ask osrs wiki api for wikiLink and imgSrc for item"""
-    params = {
-        "action": "bucket",
-        "format": "json",
-        "query": f"bucket('infobox_construction').select('page_name', 'icon').where('page_name','{input}').run()",
-    }
-    resp = s.get("https://oldschool.runescape.wiki/api.php", params=params)
-    if not resp.ok:
-        raise ConnectionError(f"request failed: {resp.status_code}")
-    if not resp.json()["bucket"]:
-        return None  # no results
-    page_name: str = resp.json()["bucket"][0]["page_name"].replace(" ", "_")
-    image_file: str = resp.json()["bucket"][0]["icon"][0].replace(" ", "_")
-    wiki_url = f"https://oldschool.runescape.wiki/w/{page_name}"
-    image_url = f"https://oldschool.runescape.wiki/w/{image_file}"
-    entry = {"wikiUrl": wiki_url, "imgUrl": image_url}
-    return entry
+def construction(name) -> dict | None:
+    """ask osrs wiki api for wikiLink and imgSrc for construction object"""
+    bucket = bucket_query("infobox_construction", name)
+    if not bucket:
+        return None
+    page_name: str = bucket[0]["page_name"].replace(" ", "_")
+    image_file: str = bucket[0]["icon"][0].replace(" ", "_")
+    wiki_url = BASE + page_name
+    image_url = BASE + image_file
+    return {"wikiUrl": wiki_url, "imgUrl": image_url}
 
 
-def skill(skill_name: str):
+def quest(quest_name: str) -> dict | None:
+    """ask osrs wiki api for wikiLink"""
+    bucket = bucket_query("quest", quest_name)
+    if not bucket:
+        return None
+    page_name: str = bucket[0]["page_name"].replace(" ", "_")
+    wiki_url = BASE + page_name
+    image_url = BASE + "images/Quest_point_icon.png"
+    return {"wikiUrl": wiki_url, "imgUrl": image_url}
+
+
+def skill(skill_name: str) -> dict | None:
     """ask osrs wiki api for wikiLink and imgSrc for skill."""
     # runecrafting is colloquially so well established some might get confused its actually called Runecraft.
-    if skill_name.capitalize() == "Runecrafting":
+    skill_name = skill_name.capitalize()
+    if skill_name == "Runecrafting":
         skill_name = "Runecraft"
-    if skill_name.capitalize() not in SKILLS:
+    if skill_name not in SKILLS:
         return None
-    wiki_url = f"https://oldschool.runescape.wiki/w/{skill_name.capitalize()}"
-    image_url = (
-        f"https://oldschool.runescape.wiki/w/File:{skill_name.capitalize()}_icon.png"
-    )
-    entry = {"wikiUrl": wiki_url, "imgUrl": image_url}
-    return entry
+    wiki_url = f"https://oldschool.runescape.wiki/w/{skill_name}"
+    image_url = BASE + "File:" + skill_name + "_icon.png"
+    return {"wikiUrl": wiki_url, "imgUrl": image_url}
 
 
-def quest(quest_name: str):
-    """ask osrs wiki api for wikiLink"""
-    params = {
-        "action": "bucket",
+def generalized_search(search: str) -> dict | None:
+    """ask osrs wiki api for wiki_url on anything. does not ask for img_url"""
+    query = {
+        "action": "opensearch",
         "format": "json",
-        "query": f"bucket('quest').select('page_name').where('page_name','{quest_name}').run()",
+        "formatversion": 2,
+        "search": search,
+        "redirects": "resolve",
     }
-    resp = s.get("https://oldschool.runescape.wiki/api.php", params=params)
-    if not resp.ok:
-        raise ConnectionError(f"request failed: {resp.status_code}")
-    if not resp.json()["bucket"]:
-        return None  # no results
-    page_name: str = resp.json()["bucket"][0]["page_name"].replace(" ", "_")
-    wiki_url = f"https://oldschool.runescape.wiki/w/{page_name}"
-    image_url = f"https://oldschool.runescape.wiki/images/Quest_point_icon.png?dc356"
-    entry = {"wikiUrl": wiki_url, "imgUrl": image_url}
-    return entry
+    resp = s.get("https://oldschool.runescape.wiki/api.php", params=query)
+    wiki_url = resp.json()[3][0]
+    image_url = BASE + "File:Null.png"
+    return {"wikiUrl": wiki_url, "imgUrl": image_url}
 
 
-# def generalized_search(search: str):
-#     """ask osrs wiki api for wiki_url on anything. does not ask for img_url"""
-#     query = {
-#         "action": "opensearch",
-#         "format": "json",
-#         "formatversion": 2,
-#         "search": search,
-#         "redirects": "resolve",
-#     }
-#     resp = s.get("https://oldschool.runescape.wiki/api.php", params=query)
-#     wiki_url = resp.json()[3][0]
-#     return wiki_url
+def slayer_rewards(name: str):
+    name = name.lower()
+
+    df = pd.read_csv("data/slayer_rewards.csv")
+    df["unlock_name"] = df["unlock_name"].map(lambda text: text.lower())
+    try:
+        image_file = (
+            df.loc[df["unlock_name"] == name, "filename"].item().replace(" ", "_")
+        )
+    except ValueError:
+        return None
+    image_url = BASE + image_file
+    wiki_url = BASE + "Slayer_Rewards"
+    return {"wikiUrl": wiki_url, "imageUrl": image_url}
 
 
-def search_all(input: str):
-    """accept just about anything"""
-    input = input.replace("'", "\\'")  # handle cases like tumeken's shadow
-    result = item(input)
-    if result:
-        return result
-    result = spell(input)
-    if result:
-        return result
-    result = construction(input)
-    if result:
-        return result
-    result = skill(input)
-    if result:
-        return result
-    result = quest(input)
-    if result:
-        return result
+def search_all(input: str) -> dict | None:
+    """iteratively attempt all queries with input, returning appropriate one or none"""
+    fns = [item, spell, construction, skill, quest, generalized_search]
+    for fn in fns:
+        result = fn(input)
+        if result:
+            return result
     return None
 
 
 if __name__ == "__main__":
-    print(quest("gertrude's cat"))
+    # print(search_all("auto-weed"))
+    print(slayer_rewards("get smashed"))
