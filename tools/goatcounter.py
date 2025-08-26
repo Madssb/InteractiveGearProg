2416293f42096b59ee29399bb9e7f51de7b12bf7
mads <anderen72@gmail.com>
2025-08-26 12:53:15 +0200

    first draft of goatcounter workflow

--- tools/goatcounter.py
+++ tools/goatcounter.py
@@ -1,24 +1,34 @@
-import os
-from datetime import date
+import argparse
+import json
+from datetime import date, timedelta
 
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
-GOATCOUNTER_KEY = os.getenv("GOATCOUNTER_KEY")
-if not GOATCOUNTER_KEY:
-    raise RuntimeError("GOATCOUNTER_KEY not set")
+print(requests.__version__)
+exit(1)
+parser = argparse.ArgumentParser()
+parser.add_argument("key")
+args = parser.parse_args()
+goatcounter_api_key = args.key
 
 base = "https://ladlor0interactive0prog.goatcounter.com/api/v0/stats/total"
-start = date(2024, 10, 23)
 end = date.today()
+start = end - timedelta(days=31)
 
 resp = requests.get(
     base,
     params={"start": start.isoformat(), "end": end.isoformat()},
-    headers={"Authorization": f"Bearer {GOATCOUNTER_KEY}"},
+    headers={"Authorization": f"Bearer {goatcounter_api_key}"},
     timeout=10,
 )
 resp.raise_for_status()
 data = resp.json()
-print("total:", data.get("total"))
+monthly_viewcount = data.get("total")
+payload = {"viewcountMonth": monthly_viewcount}
+
+with open(
+    "/home/madssb/InteractiveGearProg/data/generated/count.json",
+    mode="w",
+    encoding="utf-8",
+) as f:
+    json.dump(payload, f)
